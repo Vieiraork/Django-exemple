ce3ad663ed94aaa25cc46350951540dc3a55eca6
Vieiraork <vieiraork@gmail.com>
2021-12-03 12:47:39 -0300

    add example of post method

--- exemplos/tarefas/views.py
+++ exemplos/tarefas/views.py
@@ -1,9 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def helloWordl(request):
     return HttpResponse('Hello Wolrd!')
 
+@csrf_exempt
 def contas(request):
-    return render(request, 'tarefas/contas.html')
+    num1 = request.POST['numero1']
+    num2 = request.POST['numero2']
+        
+    soma = int(num1) + int(num2)
+        
+    return render(request, 'tarefas/contas.html', {'soma': soma, 'n1': num1, 'n2': num2})
